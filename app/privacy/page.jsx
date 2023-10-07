b8fc2dc4905858cef345341a28cf88887f373ec6
Sidd_ <siddharth726@outlook.com>
2023-10-07 00:21:39 +0000

    ngkej

--- app/privacy/page.jsx
+++ app/privacy/page.jsx
@@ -1,5 +1,11 @@
 import { redirect } from 'next/navigation'
  
+export const metadata = {
+    title: 'Privacy Policy | Elysium',
+    description: 'The privacy policy of your favorite open-source Discord AI bot.',
+  }  
+
+
 export default async function Page({ params }) {
     redirect('https://github.com/Tolga1452/elysium/blob/main/resources/privacy-policy.md')
     return (
--- app/tos/page.jsx
+++ app/tos/page.jsx
@@ -1,8 +1,13 @@
 import { redirect } from 'next/navigation'
- 
+
+export const metadata = {
+    title: 'Terms of Service | Elysium',
+    description: 'The terms of service of your favorite open-source Discord AI bot.',
+}
+
 export default async function Page({ params }) {
     redirect('https://github.com/Tolga1452/elysium/blob/main/resources/terms-of-service.md')
     return (
         <p>Redirected to the Terms of Service...</p>
     )
-  }
+}
